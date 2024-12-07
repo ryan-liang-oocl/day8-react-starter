66170a892b81f94abbd4d5cbb6f6f4a24a687fee
Ryan Liang <ryan.liang@oocl.com>
2024-12-07 16:55:08 +0800

    feat: add input validate

--- src/component/PlateNumberInput.jsx
+++ src/component/PlateNumberInput.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
 import { Input } from 'antd';
+import './css/PlateNumberInput.css';
 
 const PlateNumberInput = ({ value, onChange }) => (
-    <div style={{ marginRight: '10px' }}>
-        <label style={{ marginRight: '5px' }}>Plate Number:</label>
-        <Input value={value} onChange={onChange} style={{ width: '200px' }} />
+    <div className="plate-number-input">
+        <label className="plate-number-label">Plate Number:</label>
+        <Input
+            value={value}
+            onChange={onChange}
+            className="plate-number-input-field"
+            placeholder="input plate number"
+            pattern="^[A-Z]{2}-\d{4}$"
+            required
+        />
+        {!value && <span className="error-message">Plate number is required</span>}
+        {value && !/^[A-Z]{2}-\d{4}$/.test(value) && (
+            <span className="error-message">Wrong format</span>
+        )}
     </div>
 );
 
